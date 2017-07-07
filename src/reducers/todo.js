f083e62230009c1470f33411ed4e6f10c222c936
Javier Estévez Villa <jestevez.villa@gmail.com>
2017-07-07 21:19:49 +0200

    reducers

---
+++ src/reducers/todo.js
@@ -0,0 +1,37 @@
+const todo = (state, action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return {
+                id: action.id,
+                text: action.text,
+                completed: false
+            };
+        case 'TOGGLE_TODO':
+
+            return (state.id === action.id) ? { ...state,
+                    completed: !state.completed
+                } :
+                state
+
+
+        default:
+            return state
+    }
+};
+
+const todos = (state, action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+
+            return [...state, todo(null, action)];
+
+        case 'TOGGLE_TODO':
+
+            return state.map(item => todo(item, action))
+
+        default:
+            return state
+    }
+}
+
+export default todos
---
+++ src/reducers/todo.test.js
@@ -0,0 +1,67 @@
+import todos from './todo'
+import deepFreeze from 'deep-freeze'
+
+    test('Add todo', () =>{
+        const stateBefore = [];
+        const stateAfter = [{
+            id: 0,
+            text: 'Comprar queso',
+            completed: false
+        }];
+        const action = {
+            type: 'ADD_TODO',
+            id: 0,
+            text: 'Comprar queso'
+        }
+        deepFreeze(stateBefore);
+        deepFreeze(action);
+        expect(todos(stateBefore, action)).toEqual(stateAfter)
+    });
+
+    test('Toggle todo', () =>{
+        const stateBefore = [{
+            id: 0,
+            text: 'Comprar queso',
+            completed: false
+        },
+        {
+            id: 1,
+            text: 'Comprar tarta',
+            completed: false
+        }];
+        const stateAfter = [{
+            id: 0,
+            text: 'Comprar queso',
+            completed: false
+        },
+        {
+            id: 1,
+            text: 'Comprar tarta',
+            completed: true
+        }];
+        const action = {
+            type: 'TOGGLE_TODO',
+            id: 1
+        };
+        deepFreeze(stateBefore);
+        deepFreeze(action);
+        expect(todos(stateBefore, action)).toEqual(stateAfter)
+    });
+
+    test('wrong type', () =>{
+        const stateBefore = [];
+        const stateAfter = [{
+            id: 0,
+            text: 'Comprar queso',
+            completed: false
+        }];
+        const action = {
+            type: 'ADD_TODOOOOO',
+            id: 0,
+            text: 'Comprar queso'
+        }
+        deepFreeze(stateBefore);
+        deepFreeze(action);
+        expect(todos(stateBefore, action)).toEqual(stateBefore)
+    })
+
