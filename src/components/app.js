c2ffd95d46bc103601dd0da2fdd9804f32763bb8
Javier Estévez Villa <jestevez.villa@gmail.com>
2017-07-08 17:17:35 +0200

    creando containers

--- src/components/app.js
+++ src/components/app.js
@@ -1,17 +1,72 @@
 import React from 'react';
 
+const getVisibleItems = (todos, filter) => {
+
+  switch (filter) {
+    case 'SHOW_COMPLETED':
+      return todos.filter((item)=>item.completed);
+    case 'SHOW_PENDING':
+      return todos.filter((item)=>!item.completed);
+    default:
+      return todos;
+  }
+
+};
+
+const FilterLink = ({filter, store, current, children}) => {
+
+  if(filter === current){
+    return (<span>{children}</span>);
+  }
+  return (
+    <a href='#' onClick={(e)=>{
+          e.preventDefault();
+          store.dispatch({
+            type: 'SET_VISIBILITY_FILTER',
+            filter: filter
+          });
+        }}>
+          {children}
+    </a>
+  );
+};
+
+const Todo = ({store, text, completed, onClick}) => (
+    <li onClick={onClick}
+        style={{
+          textDecoration:
+            completed ?
+              'line-through':
+              'none'}}>
+    {text}
+  </li>
+);
+
+const TodoList = ({todos, store, onTodoClick}) => (
+
+    <ul>
+      {todos.map(item => 
+        <Todo key={item.id}
+              {...item}
+              store={store}
+              onClick={()=>onTodoClick(item.id)}
+            />
+      )}
+    </ul>
+
+);
+
 class APP extends React.Component {
-  constructor(store, todos){
-    super();
-  };
+  
   render(){
-    this.store=this.props.store;
-    this.todos=this.props.store.getState().todos;
+    const store=this.props.store;
+    const todos=this.props.store.getState().todos;
+    const visibilityFilter=this.props.store.getState().visibilityFilter;
     return (
       <div>
         <input ref={(node) => this.input=node} />
         <button onClick={() =>{
-            this.store.dispatch(
+            store.dispatch(
               {
                 type: 'ADD_TODO',
                 text: this.input.value
@@ -20,19 +75,27 @@ class APP extends React.Component {
             this.input.focus();
         }
         }>Add task</button>
-        <ul>
-          {this.todos.map(item => 
-            <li key={item.id}
-                onClick={()=>
-                  this.store.dispatch({
+        
+        <TodoList todos={getVisibleItems(todos, visibilityFilter)}
+                  store={store}
+                  onTodoClick={(id)=>
+                  store.dispatch({
                     type: 'TOGGLE_TODO',
-                    id: item.id
-                  })
-                }
-                style={{textDecoration: item.completed ? 'line-through': 'none'}}
-              >{item.id} - {item.text}</li>
-          )}
-        </ul>
+                    id: id
+                  })}/>
+
+        <FilterLink filter='SHOW_COMPLETED' store={store} current={visibilityFilter}>
+          Completed
+        </FilterLink>
+        {' '}
+        <FilterLink filter='SHOW_ALL' store={store} current={visibilityFilter}>
+          ALL
+        </FilterLink>
+        {' '}
+        <FilterLink filter='SHOW_PENDING' store={store} current={visibilityFilter}>
+          PENDING
+        </FilterLink>
+
       </div>
     );
   }
