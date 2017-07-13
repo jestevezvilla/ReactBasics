72c4131ef69c5906531be60872860d1023ab5b17
Javier Estévez Villa <jestevez.villa@gmail.com>
2017-07-13 20:00:54 +0200

    react router

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,53 +1,31 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {setVisibilityFilter} from '../actions/todo';
+import { NavLink } from 'react-router-dom';
 
-const Link =({
-  active,
-  children,
-  onClick
-}) => {
-  if(active){
-    return (<span>{children}</span>);
-  }
-  return (
-    <a href='#' onClick={(e) => {
-      e.preventDefault();
-      onClick();
-    }}>
-          {children}
-    </a>
-  );
-};
-
-
-const mapStateToProps = (state, props) => ({
-  active: props.filter === state.visibilityFilter
-});
-
-const mapDispatchToProps = (dispatch, props) => ({
-  onClick(){
-    dispatch(setVisibilityFilter(props.filter));
-  }
-});
-
-const FilterLink = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Link);
+const FilterLink = ({filter, children}) => (
+  <NavLink
+    to={'/'+filter}
+    activeStyle={{
+      textDecoration: 'none',
+      color: 'black'
+    }}
+    >
+    {children}
+  </NavLink>
+);
 
 const Footer = () => 
   (
     <div>
-      <FilterLink filter='SHOW_COMPLETED' >
+      <FilterLink filter='completed' >
           Completed
         </FilterLink>
         {' '}
-        <FilterLink filter='SHOW_ALL'>
+        <FilterLink filter='all'>
           ALL
         </FilterLink>
         {' '}
-        <FilterLink filter='SHOW_PENDING'>
+        <FilterLink filter='active'>
           PENDING
         </FilterLink>
     </div>
--- src/components/Root.js
+++ src/components/Root.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import {
+  // NOTE no more browserHistory https://reacttraining.com/react-router/web/api/BrowserRouter
+  BrowserRouter as Router,
+  Route
+  // NOTE Hello react-router-dom!
+} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 
 const Root = ({store}) => (
   <Provider store={store}>
-    <App />
+    <Router>
+      <Route path='/' component={App} />
+    </Router>
   </Provider>
 );
 
