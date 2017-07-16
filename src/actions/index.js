d2478b1932dfc55ec2ce81f35df444a18b703ff8
Javier Estévez Villa <jestevez.villa@gmail.com>
2017-07-16 11:29:14 +0200

    refacturing reducers

--- src/actions/index.js
+++
@@ -1,5 +0,0 @@
-export const counter = () => {
-  return {
-    type: 'INCREMENT'
-  };
-};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,28 @@
 import { combineReducers } from 'redux';
-import todos, * as fromTodos from './todos';
 
-const todosApp = combineReducers({
-    todos,
+import byId, * as fromById from './byId';
+import createList, * as fromList from './createList';
+
+const listByFilter =
+  combineReducers({
+    all: createList('all'),
+    active: createList('active'),
+    completed: createList('completed')
+  });
+
+const todos =
+  combineReducers({
+    byId,
+    listByFilter
 });
 
-export default todosApp;
+export default todos;
 
-export const getVisibleItems = (state, filter) =>
-  fromTodos.getVisibleItems(state.todos, filter);
+export const getVisibleItems = (state, filter) => {
+
+  const ids = fromList.getList(state.listByFilter, filter);
+  const todos = ids.map(id => fromById.getTodo(state.byId, id));
+
+  return todos;
+
+};
