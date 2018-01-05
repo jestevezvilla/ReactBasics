2e09e2b01b36e920f9db21d3a73e6cd51940e57f
jestevezvilla <jestevez.villa@gmail.com>
2018-01-05 19:33:49 +0100

    Building Lists with recompose. Adding events

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -1,95 +1,76 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import { withRouter } from 'react-router-dom';
-import * as actions from '../actions/todo';
-import { getVisibleItems, getIsFetchingItems, getIsError } from '../reducers';
-import ErrorMsg from './ErrorMsg';
+import React from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { compose, map, pipe } from "ramda";
+import * as actions from "../actions/todo";
+import { getVisibleItems, getIsFetchingItems, getIsError } from "../reducers";
+import ErrorMsg from "./ErrorMsg";
 
-const Todo = ({
-  text,
-  completed,
-  onClick
-}) => (
-    <li onClick={onClick}
-        style={{
-          textDecoration:
-            completed ?
-              'line-through':
-              'none'}}>
-    {text}
+const Todo = ({ todo, toggleTodo }) => (
+  <li
+    key={todo.id}
+    onClick={() => toggleTodo(todo.id)}
+    style={{
+      textDecoration: todo.completed ? "line-through" : "none"
+    }}
+  >
+    {todo.text}
   </li>
 );
 
-const TodoList = ({
-  todos,
-  onTodoClick
-}) => (
-    <ul>
-      {todos.map(item => 
-        <Todo key={item.id}
-              {...item}
-              onClick={()=>onTodoClick(item.id)}
-            />
-      )}
-    </ul>
+const List = todos => <ul>{todos}</ul>;
+
+const mapItems = ({ todos, toggleTodo }) =>
+  map(todo => Todo({ todo, toggleTodo }), todos);
+
+const TodoList = pipe(
+  props => ({ todos: props.todos, toggleTodo: props.toggleTodo }),
+  compose(List, mapItems)
 );
 
 class VisibleTodoList extends React.Component {
-  
-  componentDidMount(){
+  componentDidMount() {
     this.fetchData();
   }
 
-  componentDidUpdate(prevProps){
-    if(this.props.filter !== prevProps.filter){
+  componentDidUpdate(prevProps) {
+    if (this.props.filter !== prevProps.filter) {
       this.fetchData();
     }
   }
 
-  fetchData(){
+  fetchData() {
     const { fetchTodos, filter } = this.props;
     fetchTodos(filter);
   }
 
-  render(){
-    const {toggleTodo, todos, isFetching, message} = this.props;
-    
-    if(isFetching && !todos.length){
-      return (<p>Loading...</p>);
+  render() {
+    const { todos, isFetching, message } = this.props;
+
+    if (isFetching && !todos.length) {
+      return <p>Loading...</p>;
     }
 
-    if(message && !todos.length){
-      return (<ErrorMsg msg={message} onRetry={()=>this.fetchData()}/>);
+    if (message && !todos.length) {
+      return <ErrorMsg msg={message} onRetry={() => this.fetchData()} />;
     }
 
-    return(<TodoList
-              todos={todos}
-              onTodoClick={toggleTodo}
-            />);
+    return TodoList(this.props);
   }
 }
 
-const mapStateToProps = (state, {match}) => {
-  const filter = match.params.filter || 'all';;
+const mapStateToProps = (state, { match }) => {
+  const filter = match.params.filter || "all";
   return {
-    filter, 
+    filter,
     todos: getVisibleItems(state, filter),
     isFetching: getIsFetchingItems(state, filter),
     message: getIsError(state, filter)
   };
 };
 
-// const mapDispatchToProps = (dispatch) => ({
-//   onTodoClick(id){
-//     dispatch(toggleTodo(id));
-//   }
-// });
-
-VisibleTodoList = withRouter(connect(
-  mapStateToProps,
-  actions
-  //{onTodoClick: toggleTodo, receiveTodos}
-)(VisibleTodoList));
-
+VisibleTodoList = withRouter(
+  connect(mapStateToProps, actions)(VisibleTodoList)
+);
 
 export default VisibleTodoList;
